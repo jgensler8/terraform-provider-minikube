5728b3e45cd3f598eb6a956d4c5f388e26d3b63d
Jeff Gensler <jgensler8@gmail.com>
2019-08-10 11:53:41 -0700

    try running go get

--- cron.sh
+++ cron.sh
@@ -1,7 +1,11 @@
+set -ex
 echo "clone"
 git clone --depth 1 https://github.com/${TRAVIS_REPO_SLUG} /tmp/repo
-pushd /tmp/repo
-go list -u -m all
+cd /tmp/repo
+# go list -u -m all
+go get -u
+git status
+git diff go.mod
 echo "update modules"
 echo "create branch"
 echo "create PR"
