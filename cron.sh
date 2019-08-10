e7c006c487c1c3fbd00ce60bdb2d053021826d4c
Jeff Gensler <jgensler8@gmail.com>
2019-08-10 12:07:30 -0700

    see if go mod update is doing anything at all
    hard to tell what failure is causing build to fail

--- cron.sh
+++ cron.sh
@@ -3,7 +3,8 @@ echo "clone"
 git clone --depth 1 https://github.com/${TRAVIS_REPO_SLUG} /tmp/repo
 cd /tmp/repo
 # go list -u -m all
-go get -u
+# make this always work for now
+go get -u || true
 git status
 git diff go.mod
 echo "update modules"
